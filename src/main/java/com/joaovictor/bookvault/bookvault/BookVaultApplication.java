7b495c37abdcfa360c7864257382d2738047ed80
JoaoAzevedo184 <ccjconrad50@gmail.com>
2025-08-07 18:44:22 -0300

    Initial commit

---
+++ src/main/java/com/joaovictor/bookvault/bookvault/BookVaultApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictor.bookvault.bookvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookVaultApplication.class, args);
+	}
+
+}

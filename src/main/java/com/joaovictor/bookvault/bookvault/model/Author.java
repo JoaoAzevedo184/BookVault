6896c30526c4aaf76acb55a8780e7bcba7d744e9
JoaoAzevedo184 <ccjconrad50@gmail.com>
2025-08-11 22:06:42 -0300

    Adicionando atributos e métodos para autor

--- src/main/java/com/joaovictor/bookvault/bookvault/model/Author.java
+++ src/main/java/com/joaovictor/bookvault/bookvault/model/Author.java
@@ -1,4 +1,41 @@
 package com.joaovictor.bookvault.bookvault.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "autores")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int anoNascimento;
+    private Integer anoFalecimento;
+    private String status;
+
+    public Author() {}
+
+    public Author(String nome, int anoNascimento, Integer anoFalecimento) {
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.anoFalecimento = anoFalecimento;
+        this.status = (anoFalecimento == null || anoFalecimento == 0) ? "Vivo": "Morreu em " + anoFalecimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public Integer getAnoFalecimento() {
+        return anoFalecimento;
+    }
 }
